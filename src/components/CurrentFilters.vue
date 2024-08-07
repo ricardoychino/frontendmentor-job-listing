ff073323588b16691b069be2df0690dead4068fa
Ricardo Chino <ricardoychino@gmail.com>
2024-08-07 20:26:41 -0300

    Filter states component

---
+++ src/components/CurrentFilters.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { inject } from "vue";
+import BadgeSkills from "@/components/BadgeSkills.vue";
+
+const { appliedFilters, toggleFilter, clearFilter } = inject("TagFilters");
+</script>
+
+<template>
+  <div class="current-filters card">
+    <div class="applied-list">
+      <BadgeSkills v-for="filter in appliedFilters">{{ filter }}</BadgeSkills>
+    </div>
+    <a href="javascript:void(0)" @click="clearFilter()">Clear</a>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.current-filters {
+}
+</style>
--- src/components/JobList.vue
+++ src/components/JobList.vue
@@ -2,6 +2,7 @@
 import { ref, computed, Ref, provide } from "vue";
 import { useTagFilter } from "@/composables/useTagFilter";
 import JobListItem from "@/components/JobListItem.vue";
+import CurrentFilters from "@/components/CurrentFilters.vue";
 
 import type { Job } from "@/types/JobList";
 import data from "@/../data.json";
@@ -10,7 +11,7 @@ const keysToFilter = ["role", "level", "languages", "tools"];
 
 const jobs = ref<Job[]>(data);
 
-const { appliedFilters, toggleFilter, isResult } = useTagFilter(
+const { appliedFilters, toggleFilter, isResult, clearFilter } = useTagFilter(
   keysToFilter,
   data,
 );
@@ -23,11 +24,12 @@ const listed = computed(() =>
     : jobs.value.filter((job: Job) => isResult(job)),
 );
 
-provide("TagFilters", { appliedFilters, toggleFilter });
+provide("TagFilters", { appliedFilters, toggleFilter, clearFilter });
 </script>
 
 <template>
   <div class="job-list">
+    <CurrentFilters v-show="hasFilter" />
     <JobListItem v-for="job in listed" v-bind="job" :key="job.id" />
   </div>
 </template>
